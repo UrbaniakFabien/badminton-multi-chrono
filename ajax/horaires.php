b5f86db6ec51c26d10a0ec1803b1df704e4cb018
UrbaniakFabien <30938790+UrbaniakFabien@users.noreply.github.com>
2017-12-12 09:32:08 +0100

    Update horaires.php
    Correction du calcul des tranches horaires de l'échéancier :
    Prise en compte  en compte de matchs lancés en avance de plusieurs tranches horaires par rapport à la tranche en cours.
    ex : tranche en cours 10h00 et lancement d'un match de la tranche de 12h00 => avance de 2h00 (faux !)

--- ajax/horaires.php
+++ ajax/horaires.php
@@ -1,10 +1,17 @@
 <?php
 session_start();
+function convert_heure_str_minute($horaire) {
+       list($h,$m) = explode("h",$horaire);
+       return ($h*60)+$m;
+}
 /***************************************************************
  * Reprise des horaires pour mise a jour de l'échéancier avec les valeurs réélles
  * *************************************************************/ 
 $num_titre=isset($_SESSION["num_titre"])?$_SESSION["num_titre"] : 0;
  include ("../connect.7.php");
+//Liste des horaire théorique et plus petite heure de début de match pour cet horaire théorique pour les match en cours ou joués
+// +
+//Liste des horaires pour les matchs non joués qui ne sont pas dans la liste des matchs en cours ou joués
  $sql="SELECT `horaire`, min(`heure_debut`) as reel
        FROM `echeancier`
        WHERE etat in (1,2)
@@ -30,25 +37,31 @@ $num_titre=isset($_SESSION["num_titre"])?$_SESSION["num_titre"] : 0;
   $ecart=0;
   for ($i=1;$i<count($tab);$i++) {
     if ($tab[$i]["reel"]==$tab[$i]["horaire"]) {
-      //Temps réél
+      //On ne traite que les horaires pour les tranches non commencées
       
-      if ($ecart==0) {    //Calcul de l'ecart théorique entre deux tranche horaire ne se calcul qu'une fois
-          //Temps précédent
-          list($h,$m) = explode("h",$tab[$i-1]["horaire"]);
-          $t_precedent = ($h*60)+$m;
-          //Temps courant
-          list($h,$m) = explode("h",$tab[$i]["horaire"]);
-          $t_courant = ($h*60)+$m;
+      if ($ecart==0) {    //Calcul de l'ecart théorique entre deux tranches horaire  : ne se calcul qu'une fois
+          //Horaire tranche précédente
+          $t_precedent = convert_heure_str_minute($tab[$i-1]["horaire"]);
+          //Horaire tranche en cours          
+          $t_courant   = convert_heure_str_minute($tab[$i]["horaire"]);
           //Ecart par defaut
           $ecart =  $t_courant-$t_precedent;
       } 
-      
-      list($h,$m) = explode("h",$tab[$i-1]["reel"]);
-      $t_reel =($h*60)+$m;
+      $t_reel =convert_heure_str_minute($tab[$i-1]["reel"]);
       $t_reel+= $ecart;
-      
       $tab[$i]["reel"]=sprintf("%02dh%02d",(int)($t_reel/60), $t_reel%60 );
     }
+  }
+ //fur
+ //12/12/2017
+ //Recalcul en cas de besoin du début d'une tranche si elle débute avant la tranche précédente (cas de matchs lancés par avance)
+  for ($i=1;$i<count($tab);$i++) {
+     $t_reel_precedent = convert_heure_str_minute($tab[$i-1]["reel"]);
+     $t_reel_courant   = convert_heure_str_minute($tab[$i]["reel"]);
+     if ($t_reel_courant < $t_reel_precedent) {
+         $t_reel_courant = $t_reel_precedent + $ecart;
+         $tab[$i]["reel"]=sprintf("%02dh%02d",(int)($t_reel_courant/60), $t_reel_courant%60 );
+     }
   }
   echo json_encode($tab);
 ?>
