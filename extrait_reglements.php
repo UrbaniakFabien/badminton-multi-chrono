c64f5e62edecffd159b82ea85fc3b5341ea1a7bf
UrbaniakFabien <30938790+UrbaniakFabien@users.noreply.github.com>
2017-12-29 08:14:58 +0100

    Update extrait_reglements.php

--- extrait_reglements.php
+++ extrait_reglements.php
@@ -3,6 +3,10 @@
 include ("connect.7.php");
 /*
  * Extraction des données du fichier csv de réglements
+ *FUR
+ * 12/2017
+ * Annulation case 3 :
+ * Le case 4 =>enregistre
  */
 $f = fopen("reglmnt.txt", "r");
 //On vide les tables à chaque traitement
@@ -13,8 +17,9 @@ while ($ligne = fgets($f)) {
         $tab = explode(";", ($ligne));
 
         //var_dump($tab);
+        //En fonction du nombre d'éléments dans le tab on sait sur quelle type de ligne on est.
         switch (count($tab)) {
-            case 13 :
+            case 13 : //Ligne info joueur
                 if ($tab[0] != "Nom") {
                     list($j,$m,$y) = explode("/",$tab[5]);
                     $tab[5] = "$y-$m-$j";
@@ -22,16 +27,16 @@ while ($ligne = fgets($f)) {
                     $club = $tab[1];
                 }
                 break;
-            case 4 :
+            case 4 ://ligne total =>enregistrement des infos 
                 list($lib, $nbr_joueurs) = explode(":",trim( $tab[0]));
                 list($lib, $total) = explode(":", trim($tab[1]));
                 list($lib, $deja_regle) = explode(":", trim($tab[2]));
 
                 $tab_club = array("club" => $club, "nbr_joueur" => $nbr_joueurs+0, "total" => $total+0, "deja_regle" => $deja_regle+0);
                 $ok_paye =  ($total+0 ==  $deja_regle+0);
-                break;
-            case 3:
-                if (strpos($tab[0], "Ville") >= 0) {
+             //   break;
+            //case 3://si mot 'Ville' alors on change de club =>enregistrement des infos
+               // if (strpos($tab[0], "Ville") >= 0) {
                   
                     if (isset($tab_joueurs)) {
                         $sql = "INSERT INTO tbl_regl_clubs (reg_club,reg_nbr_joueurs,reg_total,reg_deja_regle) VALUES ('" . implode("','", $tab_club) . "')";
@@ -39,18 +44,18 @@ while ($ligne = fgets($f)) {
                         $result = mysqli_query($connect, $sql);
                         //reccupere aprés l'enregistrement la clef générée
                         $reg_joueurs_id_fk_club = mysqli_insert_id($connect);
-                        echo $sql . "<br>";
+                        //echo $sql . "<br>";
                         foreach ($tab_joueurs as $e_tab_joueurs) {
                             $e_tab_joueurs["reg_joueurs_regle"] = $ok_paye;
                             $sql = "INSERT INTO tbl_regl_joueurs (reg_joueurs_id_fk_club," . implode(",", array_keys($e_tab_joueurs)) . ") VALUES ($reg_joueurs_id_fk_club,'" . implode("','", $e_tab_joueurs) . "')";
-                             echo $sql . "<br>";
+                            //echo $sql . "<br>";
                             $result = mysqli_query($connect, $sql);
                             
                         }
                         unset($tab_joueurs);
                         unset($tab_club);
                     }
-                }
+               // }
                 break;
         }
     }
