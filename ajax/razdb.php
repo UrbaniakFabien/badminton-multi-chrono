535ed81345cfdaae3efc0b53fca0317fee3e5607
UrbaniakFabien <30938790+UrbaniakFabien@users.noreply.github.com>
2017-12-29 09:32:59 +0100

    Update razdb.php

--- ajax/razdb.php
+++ ajax/razdb.php
@@ -4,12 +4,23 @@
   * FU 12/2013
   * 
   *A n'utiliser que si l'on veut réinitialiser la base
+  *
+  * FUR 12/2017
+  * Suppression des tables uniquement
   **************************************************/
   session_start();
   include ("../connect.7.php");
-  $sql="DROP DATABASE ".$db;
-  mysqli_query($connect,$sql);
+//Table des tables à effacer
+$tables = ['titre','echeancier','joueurs', 'param', 'tbl_config_chrono', 'pref_titre' ,
+           'pref_param' ,'pref_tbl_config_chrono','tmp_joueurs','tbl_couleurs','tbl_regl_clubs' ,'tbl_regl_joueurs'];
+//Liste des tables à effacer
+$liste_tables = implode(",",$tables);
+//Effacement de tables
+ $sql = "DROP TABLE IF EXISTS $liste_tables";
+ mysqli_query($connect,$sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysqli_error($connect));
+//Suppression de la session
+ 
   unset($_SESSION['_login']);
   unset($_SESSION['_pass']);
-  unset($_SESSION["num_titre"]);        
-?>
+  unset($_SESSION["num_titre"]);
+  unset($_SESSION);
